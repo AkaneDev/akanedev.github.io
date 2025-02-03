52193d095eb71c28ee886ae746303a41e13adc2f
AkaneDev <akane@akanedev.au>
2025-02-03 17:49:40 +1000

    Added TTS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let isSpeaking = false;
+let isPaused = false;
+
 document.addEventListener("DOMContentLoaded", function () {
     const themeToggle = document.getElementById("theme-toggle");
     const backbutton = document.getElementById("back-button");
@@ -43,3 +46,68 @@ document.addEventListener("DOMContentLoaded", function () {
         card.style.color = "white";
     });
 });
+
+function speakText() {
+    if (!isSpeaking) {
+        isSpeaking = true;
+        var text = document.getElementById('tts').textContent;
+        document.getElementById('status').textContent = "Status: Speaking";
+        console.log("Text to speak: " + text);
+        if (text !== '') {
+        var speech = new SpeechSynthesisUtterance(text);
+        var voices = window.speechSynthesis.getVoices();
+        if (window.speechSynthesis.paused) {
+            window.speechSynthesis.resume();
+        }
+        speech.voice = voices[0]; // Select first available voice
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech);
+        } else {
+        alert("Error: No TTS section found. Please Alert the developer.");
+        }
+    }
+}
+
+function stopSpeaking() {
+    window.speechSynthesis.cancel();
+    isSpeaking = false;
+    isPaused = false;
+    document.getElementById('status').textContent = "Status: Stopped Speaking";
+}
+
+function togglepauseSpeaking() {
+    if (isSpeaking) {
+        if (isPaused) {
+            window.speechSynthesis.resume();
+            document.getElementById('status').textContent = "Status: Speaking";
+        } else {
+            window.speechSynthesis.pause();
+            document.getElementById('status').textContent = "Status: Paused";
+        }
+        isPaused = !isPaused;
+    }
+}
+
+function testSpeaking() {
+    var text = "This is a test of the text to speech system.";
+    var speech = new SpeechSynthesisUtterance(text);
+    var voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[3]; // Select first available voice
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+}
+
+function testAllVoices() {
+    var text = "test";
+    var voices = window.speechSynthesis.getVoices();
+    voices.forEach(function(voice, index) {
+        console.log("Voice " + index + ": " + voice.name);
+        var speech = new SpeechSynthesisUtterance(text);
+        speech.voice = voice;
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech);
+    });
+}
